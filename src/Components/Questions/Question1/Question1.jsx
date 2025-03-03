3100db53058286aaa09b30478ff083d0c3ed890b
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-03 13:45:35 +0000

    added title

--- src/Components/Questions/Question1/Question1.jsx
+++ src/Components/Questions/Question1/Question1.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const Question1 = ({incrementScore}) => {
+const Question1 = ({ incrementScore }) => {
   return (
     <div>
       <div className="container">
-        <div className="row" id="question" style={{ height: '30vh', backgroundColor: 'white', border: '4px solid green', padding: '20px', marginBottom: '10px', marginTop: '20px' }}>
+        <div
+          className="row"
+          id="question"
+          style={{
+            height: "30vh",
+            backgroundColor: "white",
+            border: "4px solid green",
+            padding: "20px",
+            marginBottom: "10px",
+            marginTop: "20px",
+          }}
+        >
           <div className="col-12">
-            What do you know about neurodiversity?
+            <h2>What do you know about neurodiversity?</h2>
           </div>
         </div>
-        <div className="column" id='images' style={{height: '20vh'}}></div>
-        <div className="row" id="answer" style={{ height: '20vh', backgroundColor: 'white', border: '4px solid green', padding: '20px' }}>
+        <div className="column" id="images" style={{ height: "20vh" }}></div>
+        <div
+          className="row"
+          id="answer"
+          style={{
+            height: "20vh",
+            backgroundColor: "white",
+            border: "4px solid green",
+            padding: "20px",
+          }}
+        >
           <div className="col-6">
             <Button className="w-100 mb-2">Option A</Button>
           </div>
           <div className="col-6">
-            <Button className="w-100 mb-2" onClick={incrementScore}>Option B</Button>
+            <Button className="w-100 mb-2" onClick={incrementScore}>
+              Option B
+            </Button>
           </div>
           <div className="col-6">
             <Button className="w-100">Option C</Button>
           </div>
           <div className="col-6">
-            <Button className="w-100" onClick={incrementScore}>Option D</Button>
+            <Button className="w-100" onClick={incrementScore}>
+              Option D
+            </Button>
           </div>
         </div>
       </div>
