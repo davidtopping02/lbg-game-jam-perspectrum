5cd76aa412c13d8eb9b77f6df99db0156d70f64d
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2025-03-03 12:09:46 +0000

    basic question outline

--- src/Components/Questions/Question1/Question1.jsx
+++ src/Components/Questions/Question1/Question1.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Question1 = () => {
+const Question1 = ({incrementScore}) => {
   return (
     <div>
-      <h1>Question 1</h1>
-      <p>This is the content for Question 1.</p>
+      <div className="container">
+        <div className="row" id="question">
+          <div className="col-12">
+            What do you know about neurodiversity?
+          </div>
+        </div>
+        <div className="row" id="answer">
+          <div className="col-6">
+            <Button className="w-100">Option A</Button>
+          </div>
+          <div className="col-6">
+            <Button className="w-100" onClick={incrementScore}>Option B</Button>
+          </div>
+          <div className="col-6 mt-2">
+            <Button className="w-100">Option C</Button>
+          </div>
+          <div className="col-6 mt-2">
+            <Button className="w-100">Option D</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,31 +1,38 @@
 import React, { useState } from 'react'
 import DefaultComponent from '../../Components/Default/Default'
-import { Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Question1 from '../../Components/Questions/Question1/Question1'
 
 const Home = () => {
     const [pageNumber, setPageNumber] = useState(1)
+    const [score, setScore] = useState(0)
 
     const incrementPageNumber = () => {
         setPageNumber(pageNumber + 1)
     }
 
+    const incrementScore = () => {
+      setScore(score+1)
+    }
+
     const pickCorrectPage = (pageId) => {
         switch (pageId) {
             case 1:
-                return <Question1></Question1>
+                return <Question1 incrementScore={incrementScore}></Question1>
 
             default:
                 return <DefaultComponent></DefaultComponent>
         }
     }
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">{pickCorrectPage(pageNumber)}</div>
+        <>
+            <div>Score: {score}</div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">{pickCorrectPage(pageNumber)}</div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
