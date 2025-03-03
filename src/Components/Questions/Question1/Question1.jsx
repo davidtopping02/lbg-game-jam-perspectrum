d907db8266f01cf6a1711987f7d406e1b1a54564
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2025-03-03 12:23:43 +0000

    some boxes added

--- src/Components/Questions/Question1/Question1.jsx
+++ src/Components/Questions/Question1/Question1.jsx
@@ -6,23 +6,23 @@ const Question1 = ({incrementScore}) => {
   return (
     <div>
       <div className="container">
-        <div className="row" id="question">
+        <div className="row" id="question" style={{ height: '65vh', backgroundColor: 'white', border: '1px solid black', padding: '20px', marginBottom: '10px' }}>
           <div className="col-12">
             What do you know about neurodiversity?
           </div>
         </div>
-        <div className="row" id="answer">
+        <div className="row" id="answer" style={{ height: '20vh', backgroundColor: 'white', border: '1px solid black', padding: '20px' }}>
           <div className="col-6">
-            <Button className="w-100">Option A</Button>
+            <Button className="w-100 mb-2">Option A</Button>
           </div>
           <div className="col-6">
-            <Button className="w-100" onClick={incrementScore}>Option B</Button>
+            <Button className="w-100 mb-2" onClick={incrementScore}>Option B</Button>
           </div>
-          <div className="col-6 mt-2">
+          <div className="col-6">
             <Button className="w-100">Option C</Button>
           </div>
-          <div className="col-6 mt-2">
-            <Button className="w-100">Option D</Button>
+          <div className="col-6">
+            <Button className="w-100" onClick={incrementScore}>Option D</Button>
           </div>
         </div>
       </div>
