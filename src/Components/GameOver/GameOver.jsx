2ca823bdae273e980df6fc848974d1b8e8e97c34
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-04 13:36:22 +0000

    🔱🔱🔱🔱🔱🔱🔱🔱

--- src/Components/GameOver/GameOver.jsx
+++ src/Components/GameOver/GameOver.jsx
@@ -7,11 +7,11 @@ const GameOverPage = ({ score }) => {
   const { width, height } = useWindowSize();
   return (
     <>
-      <Confetti width={width} height={height} />
       <div className="container text-center d-flex flex-column justify-content-center align-items-center vh-100">
         <h1 className="fw-bold">Game Over</h1>
         <h1>Your Score: {score} / 7</h1>
       </div>
+      <Confetti width={width} height={height} />
     </>
   );
 };
